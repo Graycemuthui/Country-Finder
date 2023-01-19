ba67c45184cde40cd5c3d5af662288da7582d7f2
Grayce Muthui <graycemuthui20@gmail.com>
2023-01-19 14:34:45 +0300

    Fetch data from the Api

---
+++ src/redux/Countries/displayCountries.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+// constant for action
+const FETCH_COUNTRIES = "CountryFinder/Countries/fetchCountries";
+// const SEARCH_COUNTRY = "./displayCountries/SEARCH_COUNTRY";
+// const FILTER_COUNTRY = "./displayCounrties/FILTER_COUNTRY";
+const BASE_URL = "https://restcountries.com/v3.1/all";
+
+// reducer
+export default function countriesReducer(state = [], action) {
+  switch (action.type) {
+  }
+}
+
+// Action creator
+export const fetchCountries = async (dispatch) => {
+  try {
+    const response = await axios.get(BASE_URL);
+    dispatch({
+      type: FETCH_COUNTRIES,
+      payload: response.data,
+    });
+    return response.data;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+// export const searchBook = (names) async(dispatch) => {
+// await axios.filter((name) => {
+// if (search === "") {
+// return;
+// }else if ()
+// })
+// }
